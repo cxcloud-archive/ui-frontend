c193ca0e565905b976fd830170dac2ba9339c85b
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-11-16 16:14:25 +0200

    Initial commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: false,
+  siteName: 'CXCloud DEV',
+  commerce: {
+    apiUrl: 'https://dev.cxcloud.com/api/v1',
+    indexName: 'dev_COMMERCE'
+  },
+  content: {
+    apiUrl: 'https://dev.cxcloud.com/api/v1/content',
+    indexName: 'dev_CONTENT'
+  },
+  auth: {
+    apiUrl: 'https://dev.cxcloud.com/api/v1/auth'
+  },
+  search: {
+    apiUrl: 'https://dev.cxcloud.com/api/v1/search'
+  }
+};
